036521d3886cb8d363926422ff2e9844c6ba2595
Shinji Fujimoto <fujimoto.shinji@classmethod.jp>
2016-10-24 01:14:57 +0900

    test

--- app/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-
-class AppTest(TestCase):
-    def test_add(self):
-        self.assertEqual(1 + 1, 2)
--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^app/', include('app.urls')),
-    url(r'^news/', include('news.urls')),
 ]
