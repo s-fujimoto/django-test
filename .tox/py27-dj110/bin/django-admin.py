a59df05f509a841cdb7c29a0b2b1479aa9a64e42
Shinji Fujimoto <fujimoto.shinji@classmethod.jp>
2016-10-24 00:22:46 +0900

    test

--- .tox/py27-dj110/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/fujimoto.shinji/Techs/python/django-test/.tox/py27-dj110/bin/python2.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
